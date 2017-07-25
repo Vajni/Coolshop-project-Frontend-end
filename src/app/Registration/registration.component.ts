4a1285c5704a8175b21648c0efa94f2d2dd7ec92
Szabó Szilveszter <szaboszilveszter0909@gmail.com>
2017-07-25 16:02:03 +0200

    Must fill out all fields & if passwords are not the same get error.

--- src/app/Registration/registration.component.ts
+++ src/app/Registration/registration.component.ts
@@ -5,4 +5,30 @@ import { Component } from '@angular/core';
     templateUrl: 'registration.component.html',
     styleUrls: ['registration.component.css']
 })
-export class RegistrationComponent{}
+export class RegistrationComponent {
+
+    username: string;
+    email: string;
+    password: string;
+    passwordagain: string;
+    phonenumber: string;
+
+    country: string;
+    region: string;
+    postalcode: string;
+    city: string;
+    streetandstreetnumber: string;
+
+    registration(username, email, password, passwordagain, phonenumber, country, region, postalcode, city, streetandstreetnumber) {
+        if (username==null || email==null || password==null || passwordagain==null || phonenumber==null || country==null || region==null || postalcode==null || city==null || streetandstreetnumber==null) {
+        alert("You must fill out all field.");
+        } else if (password != passwordagain) {
+            alert("Passwords are not the same.")
+        } else {
+            alert("Everything is ok.")
+            console.log(username);
+        }
+    }
+
+
+}
