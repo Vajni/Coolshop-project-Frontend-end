6109f51c418440ab282f33c02a55455bbb018743
FerencOnodi <onodiferenc91@gmail.com>
2017-07-25 14:53:35 +0200

    Added: user.ts - mock data to work with Modified: rm.ts/rm.html - displaying user data in a table

--- src/app/RoleManagement/rolemanagement.component.ts
+++ src/app/RoleManagement/rolemanagement.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+import { User } from './user';
+
+const USERS: User[] = [
+    {name: 'TestUser', role: 'user'},
+    {name: 'TestMerchant', role: 'merchat'},
+    {name: 'TestAdmin', role: 'admin'}
+];
+
 @Component({
     selector: 'rolemanagement',
     templateUrl: './rolemanagement.component.html',
@@ -7,4 +15,5 @@ import { Component } from '@angular/core';
 })
 
 export class RoleManagementComponent {
+    users = USERS;
 }
