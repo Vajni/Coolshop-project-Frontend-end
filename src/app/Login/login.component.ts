74fff3a98ca6572687f97ffe3f4d6a0e507234ba
ThomasKenyeres <kenyerest97@gmail.com>
2017-07-26 13:56:14 +0200

    Login page template created

--- src/app/Login/login.component.ts
+++ src/app/Login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'login',
-  template: "<h1>Login</h1><hr>This is the login page!"
+  templateUrl: '../Login/login.component.html'
+  //template: "<h1>Login</h1><hr>This is the login page!"
 })
 export class LoginComponent {
-
+  backgroundColor: string = "c45f2ad";
 }
