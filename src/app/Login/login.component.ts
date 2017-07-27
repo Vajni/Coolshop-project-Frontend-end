66d15bca9b4326e0ba498a8a58df2e0485916379
ThomasKenyeres <kenyerest97@gmail.com>
2017-07-27 17:37:26 +0200

    Login implemented(temp. without session)

--- src/app/Login/login.component.ts
+++ src/app/Login/login.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { LoginService } from './login.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'login',
-  templateUrl: '../Login/login.component.html'
+  templateUrl: '../Login/login.component.html',
+  providers: [LoginService]
   //template: "<h1>Login</h1><hr>This is the login page!"
 })
 export class LoginComponent {
   backgroundColor: string = "c45f2ad";
+  email: string;
+  password: string;
+  localUser = {
+    email: "",
+    password: ""
+  }
+
+  constructor(private _service: LoginService, private _router: Router) {
+
+  }
+
+  sendClicked() {
+    this._service.login(this.email, this.password);
+    //this._service.loginfn({email: this.email, password: this.password}).then((res) => {})
+    //alert(this._httpService.getLogin());
+    //alert("fasz: " + this.email + " : " + this.password);
+    //return this._http.post("http://localhost:8080/service/user/login", '{"asd": "asd"}');
+  }
 }
