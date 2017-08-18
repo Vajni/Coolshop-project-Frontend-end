58a58f09c055b29d6f3b756cfc3828316aea1485
ThomasKenyeres <kenyerest97@gmail.com>
2017-08-18 09:31:44 +0200

    Login/out -show on navbar - impl. started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,42 @@ import { ProductService } from './Products/product.service';
   providers : [ProductService]
 })
 export class AppComponent{
-  
+  static texts = {
+    "logged_out": "Sign in",
+    "logged_in": "Sign out"
+  };
+  static urls = {
+    "logged_out": "/login",
+    "logged_in": ""
+  };
+  static clickfuncs = {
+    "logged_out": "",
+    "logged_in": "(click=\"logout()\")"
+  };
+
+  randomtext = "asd";
+  static clickfunc = AppComponent.clickfuncs["logged_out"];
+  static userButtonText = AppComponent.texts["logged_out"];
+  static userButtonUrl = AppComponent.urls["logged_out"];
+
+  get staticClickFunc(): string {
+    return AppComponent.clickfunc;
+  }
+
+  get staticUserButtonText(): string {
+    return AppComponent.userButtonText;
+  }
+
+  get staticUserButtonUrl(): string {
+    return AppComponent.userButtonUrl;
+  }
+
+  static login() {
+
+  }
+
+  static logout() {
+
+  }
+
 }
