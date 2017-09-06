f09ddcf1c80b902b20b2e51d61de1efc1dc7c815
Szabó Szilveszter <szaboszilveszter0909@gmail.com>
2017-09-06 12:41:25 +0200

    Delete console.log() line from product.service.ts.

--- src/app/Products/product.service.ts
+++ src/app/Products/product.service.ts
@@ -26,7 +26,6 @@ export class ProductService {
     }
       
     private handleError(error: Response){
-        console.error(error);
         return Observable.throw(error.json().error || "Server error");
     }
 
